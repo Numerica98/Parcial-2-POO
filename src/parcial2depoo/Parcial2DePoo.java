d5ddbadcf25639b6cdb0043874fb8f268a46e747
Nuria Rivas <00041517@uca.edu.sv>
2018-06-06 21:48:43 -0300

    Se agrego una introduccion al juego

--- src/parcial2depoo/Parcial2DePoo.java
+++ src/parcial2depoo/Parcial2DePoo.java
@@ -5,6 +5,8 @@
  */
 package parcial2depoo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Melissa
@@ -15,7 +17,21 @@ public class Parcial2DePoo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("¡¡¡¡¡BIENVENIDOS A NURIARIVAS'S WORD!!!!!");
+        System.out.println("Donde todo puede pasar en el campo de batalla");
+        System.out.println("Pueden elegir pertenecer al bando de: Los Duendes y Hadas, Los Hechiceros y Los Animan");
+        System.out.println("Así que ¡no esperen mas! Ingresen sus nombres y la raza a escoger por favor");
+        Scanner dato= new Scanner(System.in);
+        System.out.println("------JUGADOR 1-----");
+        System.out.println("Nombre: ");
+        String jugador1 = dato.nextLine();
+        System.out.println("Raza: ");
+        String razaJugador1= dato.nextLine();
+        System.out.println("------JUGADOR 2-----");
+        System.out.println("Nombre: ");
+        String jugador2 = dato.nextLine();
+        System.out.println("Raza: ");
+        String razaJugador2= dato.nextLine();
     }
     
 }
