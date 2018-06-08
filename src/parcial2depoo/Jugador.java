d6a428e3ff208251591659ded24db6da06b7b334
Nuria Rivas <00041517@uca.edu.sv>
2018-06-08 10:45:38 -0300

    Se agrego la clase jugador y menu para el jugador

---
+++ src/parcial2depoo/Jugador.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial2depoo;
+
+/**
+ *
+ * @author Melissa
+ */
+public class Jugador {
+    private String nombre;
+    private String raza;
+    
+}
---
+++ src/parcial2depoo/MenuDeJugador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial2depoo;
+
+/**
+ *
+ * @author Melissa
+ */
+public class MenuDeJugador {
+    private static void Menu(){
+        
+    }
+    
+}
